3eb7ec1b26ae69d4b71ecdab1e004a0873d9c377
Andrew Millington <andrew@noexceptions.io>
2020-10-01 10:37:28 +0100

    Remove getClaims and addClaim from TokenInterface

--- src/Entities/TokenInterface.php
+++ src/Entities/TokenInterface.php
@@ -75,18 +75,4 @@ interface TokenInterface
      * @return ScopeEntityInterface[]
      */
     public function getScopes(): array;
-
-    /**
-     * Associate a claim with the token.
-     *
-     * @param ClaimEntityInterface $claim
-     */
-    public function addClaim(ClaimEntityInterface $claim);
-
-    /**
-     * Return an array of claims associated with the token.
-     *
-     * @return ClaimEntityInterface[]
-     */
-    public function getClaims(): array;
 }
